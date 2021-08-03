557290a742e12acbb15de73408cc7a39372b5af8
bingoootang <bingooo.tang@gmail.com>
2021-08-03 12:47:10 +0800

    feat: graphql 网关

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { GraphQLGatewayModule } from '@nestjs/graphql';
+
+const graphqlGatewayModule = GraphQLGatewayModule.forRootAsync({
+  useFactory: async () => ({
+    server: {
+      path: '/bff/graphql',
+    },
+    gateway: {
+      serviceList: [
+        {
+          name: 'goods',
+          url: 'http://localhost:3001/goods/graphql',
+        },
+        {
+          name: 'order',
+          url: 'http://localhost:3002/order/graphql',
+        },
+      ],
+    },
+  }),
+});
+
+@Module({
+  imports: [graphqlGatewayModule],
+})
+export class AppModule {}
